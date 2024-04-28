47f3591e6178ab3fe50b3c06f749673bb25efb6d
isa <isa430752@gmail.com>
2024-04-29 02:50:11 +0300

    edit

--- main.js
+++ main.js
@@ -10,36 +10,18 @@ action.classList.toggle("is-active")
 }
 
 
-window.addEventListener('scroll', function() {
-  var navbar = document.getElementById('header');
-  if (window.scrollY > 200) {
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
+  window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('header');
+    if (window.scrollY > 200) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
 
 
 
 
-  // function filterWorks(category, link) {
-  //   var links = document.querySelectorAll('.links a');
-  //   links.forEach(function(link) {
-  //     link.classList.remove('active');
-  //   });
-
-  //   link.classList.add('active');
-
-  //   var cards = document.querySelectorAll('.box img');
-  //   cards.forEach(function(card) {
-  //     var categoryClass = card.getAttribute('data-category');
-  //     if (category === 'all' || category === categoryClass) {
-  //       card.style.display = 'block';
-  //     } else {
-  //       card.style.display = 'none';
-  //     }
-  //   });
-  // }
 
   function filterWorks(category) {
     // Remove 'active' class from all links
@@ -61,3 +43,8 @@ window.addEventListener('scroll', function() {
       }
     });
   }
+
+
+
+
+
